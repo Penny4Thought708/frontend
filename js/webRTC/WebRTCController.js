569c68e8aaab517671e4d97fed3f6c7fdf913d88
Penny4Thought708 <Haroldgore40@gmail.com>
2026-01-20 14:17:43 -0500

    Update WebRTCController.js

--- js/webRTC/WebRTCController.js
+++ js/webRTC/WebRTCController.js
@@ -23,7 +23,7 @@ import {
   localWrapper,
   remoteWrapper,
 } from "../session.js";
-import { getIceServers } from "../ice.js";
+import { getIceServers } from "/ice.js";
 import { getReceiver } from "../messaging.js";
 
 /* -------------------------------------------------------
