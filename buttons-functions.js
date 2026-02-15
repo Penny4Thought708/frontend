53e7bc1b4cd91577401d7508e491e3d1451e316e
Penny4Thought708 <Haroldgore40@gmail.com>
2026-02-14 23:32:00 -0500

    Update buttons-functions.js

--- buttons-functions.js
+++ buttons-functions.js
@@ -129,9 +129,10 @@ function initUI() {
       // 🔹 Voicemail section hook
     
 if (sectionName === "voicemail") {
-  import("./public/js/voicemail/VoicemailUi.js").then(({ loadVoicemails }) => {
-    loadVoicemails();
-  });
+ import("/frontend/public/js/voicemail/VoicemailUi.js").then(({ loadVoicemails }) => {
+  loadVoicemails();
+});
+
 }
 
 
@@ -328,6 +329,7 @@ const FloatingWindows = {
     });
   }
 };
+
 
 
 
